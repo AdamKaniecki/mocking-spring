3192913f17f9bcb38fd731a7be0e8f2ab4c8e7c9
Adam Kaniecki <adam2113@poczta.onet.pl>
2024-02-11 12:35:40 +0100

    metoda statyczna w testach na przykładzie LocalTime

--- src/main/java/org/example/InjectedService.java
+++
@@ -1,6 +0,0 @@
-package org.example;
-
-public interface InjectedService {
-    String firstMethod();
-    String thirdMethod();
-}
--- src/main/java/org/example/ServiceWithDependency.java
+++
@@ -1,7 +0,0 @@
-package org.example;
-
-public interface ServiceWithDependency {
-
-    void setInjectedService(InjectedService injectedService);
-    String secondMethod();
-}
